e79d1757795885eda96135a1088776cb6868d4c4
Hide <padocon@naver.com>
2022-04-25 00:52:06 +0900

    Replace Field to UserSchema

--- app/user/schema/user.py
+++ app/user/schema/user.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserSchema(BaseModel):
-    id: int = None
-    password: str = None
-    email: str = None
-    nickname: str = None
-    is_admin: bool = None
-    created_at: datetime = None
-    updated_at: datetime = None
+    id: int = Field(None, description="ID")
+    password: str = Field(None, description="Password")
+    email: str = Field(None, description="Email")
+    nickname: str = Field(None, description="Nickname")
+    is_admin: bool = Field(None, description="Is admin")
+    created_at: datetime = Field(None, description="Create Time")
+    updated_at: datetime = Field(None, description="Update Time")
 
     class Config:
         orm_mode = True
